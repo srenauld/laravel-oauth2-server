475fc1fa7c13faff6ee289e442443473cb88b072
srenauld <seb@sebrenauld.co.uk>
2013-03-27 15:40:07 +0000

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,1 +1,35 @@
-<?phpRoute::post("(:bundle)/authorize", function() {	$oA = new SebRenauld\OAuth2();	$i = Input::all();	$r = $oA->finishClientAuthorization($i);	return $r;});Route::get("(:bundle)/authorize", function() {	$oA = new SebRenauld\OAuth2();	$i = Input::all();	$r = $oA->getAuthorizeParams($i);	if (!is_array($r)) return $r;	return View::make("oauth2-server::form",array("data" => $r));});Route::get("(:bundle)/token", function() {	$oA = new SebRenauld\OAuth2();	$r = $oA->grantAccessToken(Input::all());	if ($r instanceof Laravel\Response) {		return $r;	}	if ($r instanceof SebRenauld\OAuth2\Models\Token) {		return $r->printToken();	}});Route::post("(:bundle)/token", function() {	$oA = new SebRenauld\OAuth2();	$r = $oA->grantAccessToken(Input::all());	if ($r instanceof Laravel\Response) {		return $r;	}	if ($r instanceof SebRenauld\OAuth2\Models\Token) {		return $r->printToken();	}});
+<?php
+Route::post("(:bundle)/authorize", function() {
+	$oA = new SebRenauld\OAuth2();
+	$i = Input::all();
+	$r = $oA->finishClientAuthorization($i);
+	return $r;
+});
+Route::get("(:bundle)/authorize", function() {
+	$oA = new SebRenauld\OAuth2();
+	$i = Input::all();
+	$r = $oA->getAuthorizeParams($i);
+	if (!is_array($r)) return $r;
+	return View::make("oauth2-sp::form",array("data" => $r));
+});
+Route::get("(:bundle)/token", function() {
+	$oA = new SebRenauld\OAuth2();
+	$r = $oA->grantAccessToken(Input::all());
+	if ($r instanceof Laravel\Response) {
+		return $r;
+	}
+	if ($r instanceof SebRenauld\OAuth2\Models\Token) {
+		return $r->printToken();
+	}
+});
+
+Route::post("(:bundle)/token", function() {
+	$oA = new SebRenauld\OAuth2();
+	$r = $oA->grantAccessToken(Input::all());
+	if ($r instanceof Laravel\Response) {
+		return $r;
+	}
+	if ($r instanceof SebRenauld\OAuth2\Models\Token) {
+		return $r->printToken();
+	}
+});
